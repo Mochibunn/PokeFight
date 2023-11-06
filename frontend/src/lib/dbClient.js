ce85fa23527f0a4c4a34b21c5386b3fabec9f5be
Mochi McCoy <hi@mochibun.me>
2023-11-06 16:48:55 +0100

    Frontend work, WIP

---
+++ frontend/src/lib/dbClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+const backend = import.meta.env.VITE_BACKEND;
+
+const getAllPokemon = async () => {
+  try {
+    const { data } = await axios.get(`${backend}/pokefight/pokemon`);
+    console.log("🟢🐰 Pokémon fetched!");
+    return data;
+  } catch (error) {
+    console.error(`🛑🐰 Failed to fetch pokémon`, error);
+  }
+};
+
+const getSinglePokemon = async (id) => {
+  try {
+    if (!+id) throw new Error(`🔢 ID must be an integer value`);
+    const { data } = await axios.get(`${backend}/pokefight/pokemon/${id}`);
+    return data;
+  } catch (error) {
+    return `🛑🐰 Ack! An error! `, error;
+  }
+};
+
+export { getAllPokemon, getSinglePokemon };
