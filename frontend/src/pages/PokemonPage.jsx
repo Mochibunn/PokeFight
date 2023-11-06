7274a8dc1de6711bcd13bd1b67406bee94dbeb46
Mochi McCoy <hi@mochibun.me>
2023-11-06 17:01:30 +0100

    Undo stash clear

--- frontend/src/pages/PokemonPage.jsx
+++ frontend/src/pages/PokemonPage.jsx
@@ -1,7 +1,25 @@
-export default function PokemonPage(){   
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getSinglePokemon } from "../lib/dbClient";
 
-    return (
-        <>
-        </>
-    )
-}
+const PokemonPage = () => {
+  const { id } = useParams();
+  const [pokemon, setPokemon] = useState({});
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    try {
+      if (!+id) throw new Error(`🔢 ID must be an integer value`);
+      getSinglePokemon(id).then((data) => {
+        console.log(`🟢🐰 Single pokémon fetched!`);
+        setPokemon(data);
+      });
+    } catch (error) {
+      console.error(`🛑🐰 Oops, that's an error!\n`, error.message);
+    }
+  }, [id]);
+
+  return <></>;
+};
+
+export default PokemonPage;
